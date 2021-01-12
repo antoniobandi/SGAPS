28328d0e8efa7fbbf6c9904c3427fe8cd18cb5e8
lbalic21 <72878065+lbalic21@users.noreply.github.com>
2021-01-12 15:11:07 +0100

    Add files via upload

---
+++ Core/Src/lcd.c
@@ -0,0 +1,270 @@
+
+#include "lcd.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int width;
+
+
+void StartScreen(void){
+	TM_LCD_Init();
+	TM_LCD_SetOrientation(2);
+	TM_LCD_SetLayer1();
+	TM_LCD_Fill(LCD_COLOR_WHITE);
+
+
+	ScreenIntro();
+
+	/*	prozor za osciloskop	*/
+
+	Window();
+
+	/*	generator; ime i ON/OFF */
+
+	TM_LCD_SetXY(0, TM_LCD_GetHeight()-22);
+	TM_LCD_SetFont(&TM_Font_7x10);
+	char s[] = "Function generator";
+	TM_LCD_Puts(&s[0]);
+	char on[] = "ON";
+	TM_LCD_SetXY(8, TM_LCD_GetHeight()-11);
+	TM_LCD_Puts(&on[0]);
+	char off[] = "OFF";
+	TM_LCD_SetXY(28, TM_LCD_GetHeight()-11);
+	TM_LCD_Puts(&off[0]);
+
+	/*	prozor za generator	*/
+
+	for(int i=135; i<160;i++){
+		TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-22, LCD_COLOR_BLACK);
+	}
+	for(int i=135; i<161;i++){
+			TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-5, LCD_COLOR_BLACK);
+		}
+	for(int i=TM_LCD_GetHeight()-22; i<TM_LCD_GetHeight()-5;i++){
+			TM_LCD_DrawPixel(135, i, LCD_COLOR_BLACK);
+		}
+	for(int i=TM_LCD_GetHeight()-22; i<TM_LCD_GetHeight()-5;i++){
+			TM_LCD_DrawPixel(160, i, LCD_COLOR_BLACK);
+		}
+	char yaxis[] = "0.15 V/div";
+	TM_LCD_SetXY(210,TM_LCD_GetHeight()*3/4+5);
+	TM_LCD_Puts(&yaxis[0]);
+}
+
+void ScreenIntro(void){
+	char begin[] = "Sustav za generiranje,";
+	char end[] = "akviziciju i prikaz signala";
+	TM_LCD_SetXY(35, TM_LCD_GetHeight()/2-22);
+	TM_LCD_Puts(&begin[0]);
+	TM_LCD_SetXY(10, TM_LCD_GetHeight()/2);
+	TM_LCD_Puts(&end[0]);
+	char fer[] = "FER, 2020./2021.";
+	TM_LCD_SetFont(&TM_Font_7x10);
+	TM_LCD_SetXY(8, TM_LCD_GetHeight()-15);
+	TM_LCD_Puts(&fer[0]);
+	for(int i=0; i<20000000;i++){}
+	char next1[] = "Press user button";
+	char next2[] = "to continue";
+	TM_LCD_SetXY(200, TM_LCD_GetHeight()-20);
+	TM_LCD_Puts(&next1[0]);
+	TM_LCD_SetXY(200, TM_LCD_GetHeight()-10);
+	TM_LCD_Puts(&next2[0]);
+	while(1){
+		if(TM_DISCO_ButtonPressed() > 0) {
+			TM_LCD_Fill(LCD_COLOR_WHITE);
+			break;
+		}
+	}
+
+}
+
+void LCDFunctionGeneratorON(void){
+	for(int i=7;i<23;i++){
+		for(int j=TM_LCD_GetHeight()-13;j<TM_LCD_GetHeight()-2;j++){
+			if(TM_LCD_GetPixel(i, j) == LCD_COLOR_WHITE){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_GREEN2);
+			}
+		}
+	}
+	for(int i=27;i<49;i++){
+		for(int j=TM_LCD_GetHeight()-13;j<TM_LCD_GetHeight()-2;j++){
+			if(TM_LCD_GetPixel(i, j) == LCD_COLOR_RED2){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+			}
+		}
+	}
+}
+
+void LCDFunctionGeneratorOFF(void){
+	for(int i=27;i<49;i++){
+		for(int j=TM_LCD_GetHeight()-13;j<TM_LCD_GetHeight()-2;j++){
+			if(TM_LCD_GetPixel(i, j) == LCD_COLOR_WHITE){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_RED2);
+			}
+		}
+	}
+	for(int i=7;i<23;i++){
+		for(int j=TM_LCD_GetHeight()-13;j<TM_LCD_GetHeight()-2;j++){
+			if(TM_LCD_GetPixel(i, j) == LCD_COLOR_GREEN2){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+			}
+		}
+	}
+	for(int i=137;i<159;i++){
+		for(int j=TM_LCD_GetHeight()-18;j<TM_LCD_GetHeight()-7;j++){
+			TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+		}
+	}
+	char gasi[] = "               ";
+	TM_LCD_SetXY(165, TM_LCD_GetHeight()-18);
+	TM_LCD_Puts(&gasi[0]);
+}
+
+void SendToScreen(int* signal){
+	for(int i=0;i<BUFFER_SIZE;i++){
+		int amplitude = -0.04297924298 * signal[i] + 180;
+		TM_LCD_DrawPixel(width, amplitude, LCD_COLOR_YELLOW );
+		width++;
+		if(width == 318){
+			width = 1;
+			Window();
+		}
+	}
+}
+
+void SetSquareOutput(long int freq){
+	LCDFunctionGeneratorON();
+	double sinus[20];
+	for(int i=0; i<20; i++){
+		double a = (double) i;
+		sinus[i] = sin(0.33*a);
+	}
+
+	for(int i=0;i<20;i++){
+		int amplitude = (int)(-6* sinus[i] + 227);
+		TM_LCD_DrawPixel(i+138, amplitude, LCD_COLOR_WHITE );
+	}
+	for(int i=147; i<159;i++){
+		TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-18, LCD_COLOR_RED2);
+	}
+	for(int i=137; i<147;i++){
+		TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-8, LCD_COLOR_RED2);
+	}
+	for(int i=TM_LCD_GetHeight()-18; i<TM_LCD_GetHeight()-8;i++){
+		TM_LCD_DrawPixel(147, i, LCD_COLOR_RED2);
+	}
+	char f[10] = {0};
+	sprintf(f,"%.ld",freq);
+	for(int i=165;i<320;i++){
+		for(int j=TM_LCD_GetHeight()-18;j<TM_LCD_GetHeight();j++){
+			TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+		}
+	}
+	TM_LCD_SetXY(165, TM_LCD_GetHeight()-18);
+	TM_LCD_Puts(&f[0]);
+	char f1[] = " Hz";
+	TM_LCD_Puts(&f1[0]);
+
+}
+
+void SetSineOutput(long int freq){
+
+	LCDFunctionGeneratorON();
+	double sinus[20];
+	for(int i=147; i<159;i++){
+		TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-18, LCD_COLOR_WHITE);
+	}
+	for(int i=137; i<147;i++){
+		TM_LCD_DrawPixel(i, TM_LCD_GetHeight()-8, LCD_COLOR_WHITE);
+	}
+	for(int i=TM_LCD_GetHeight()-18; i<TM_LCD_GetHeight()-8;i++){
+		TM_LCD_DrawPixel(147, i, LCD_COLOR_WHITE);
+	}
+
+	for(int i=0; i<20; i++){
+		double a = (double) i;
+		sinus[i] = sin(0.33*a);
+	}
+
+	for(int i=0;i<20;i++){
+		int amplitude = (int)(-6* sinus[i] + 227);
+		TM_LCD_DrawPixel(i+138, amplitude, LCD_COLOR_RED2);
+	}
+	char f[10] = {0};
+	sprintf(f,"%.ld",freq);
+	for(int i=165;i<320;i++){
+			for(int j=TM_LCD_GetHeight()-18;j<TM_LCD_GetHeight();j++){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+			}
+		}
+	TM_LCD_SetXY(165, TM_LCD_GetHeight()-18);
+	TM_LCD_Puts(&f[0]);
+	char f1[] = " Hz";
+	TM_LCD_Puts(&f1[0]);
+}
+
+void ShowSignal(int* signal, char title[]){
+	for(int i=1; i<319;i++){
+		for(int j=1;j<TM_LCD_GetHeight()*3/4;j++){
+			if(TM_LCD_GetPixel(i, j) == LCD_COLOR_YELLOW){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_BLACK);
+			}
+		}
+	}
+	for(int i=1;i<110;i++){
+		for(int j=185;j<205;j++){
+			TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+		}
+	}
+	TM_LCD_SetFont(&TM_Font_11x18);
+	TM_LCD_SetXY(1,TM_LCD_GetHeight()*3/4+5);
+	TM_LCD_Puts(&title[0]);
+	TM_LCD_SetFont(&TM_Font_7x10);
+	SendToScreen(signal);
+}
+
+void Window(void){
+	for(int i=1; i<319;i++){
+			for(int j=1;j<TM_LCD_GetHeight()*3/4+2;j++){
+				TM_LCD_DrawPixel(i, j, LCD_COLOR_BLACK);
+			}
+		}
+		for(int i=1; i<319;i++){
+			for(int j=1;j<TM_LCD_GetHeight()*3/4;j++){
+				if(j==91){
+					if(i%3==0){
+						TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+					}
+				}
+				if((i%16==0)&&(j%9==0))	TM_LCD_DrawPixel(i, j, LCD_COLOR_WHITE);
+			}
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Core/Src/signal_matlab.c
@@ -0,0 +1,145 @@
+#include "fft.h"
+#include "usart.h"
+#include "signal_matlab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <complex.h>
+
+bool gen = GEN_S;
+bool sinewave = SINE_S;
+bool signal_q = SIG_S;
+bool filtered = FIL_S;
+bool lowpass = LOWPASS_S;
+long int amplitude_temp = AMP_TEMP;
+float amplitude = AMP_S;
+long int frequency = FREQ_S;
+double cutOffFreq = FGFREQ_S;
+
+char c;
+char *ptr;
+char temp[2] = {'-', '\0'};
+
+float i = 0;
+
+
+
+void matlab(void){
+
+	char response[RESPONSE_SIZE];
+	response[0] = '-';
+	response[1] = '\0';
+	
+	while (USART1_Dequeue(&c)){
+		temp[0] = c;
+		strncat(response,temp,1);
+	}
+
+	switch (response[1]){
+		
+		case '\0':
+			break;
+
+		case 'G' :
+			if (strncmp(":on",strchr(response, ':'),3) == 0 ){
+				gen = true;
+			}
+			else if (strncmp(":off", strchr(response,':'),4) == 0){
+				gen = false;
+			}
+			break;
+
+		case 'W':
+			if (strncmp(":Sin",strchr(response, ':'),4) == 0 ){
+				sinewave = true;
+			}
+			else if (strncmp(":Pra", strchr(response,':'),4) == 0){
+				sinewave = false;
+			}
+			break;
+
+		case 'F':
+			frequency = strtol(strchr(response,'0'),&ptr,10);
+			if (frequency <= 0) frequency = FREQ_MIN;
+			break;
+
+		case 'A':
+			amplitude_temp = strtol(strchr(response,'0'),&ptr,10);
+			amplitude = amplitude_temp * AMP_COEF;
+			if (amplitude < 0) amplitude = AMP_MIN;
+			break;
+
+		case 'P':
+			if (strncmp(":low",strchr(response, ':'),4) == 0 ){
+				lowpass = true;
+			}
+			else if (strncmp(":high", strchr(response,':'),5) == 0){
+				lowpass = false;
+			}
+			break;
+
+		case 'S':
+			if (strncmp(":Sig",strchr(response, ':'),4) == 0 ){
+				signal_q = true;
+			}
+			else if (strncmp(":Spec", strchr(response,':'),5) == 0){
+				signal_q = false;
+			}
+			break;
+
+		case 'L':
+			if (strncmp(":Yes",strchr(response, ':'),4) == 0 ){
+				filtered = true;
+			}
+			else if (strncmp(":No", strchr(response,':'),3) == 0){
+				filtered = false;
+			}
+			break;
+
+		case 'Q':
+			gen = GEN_S;
+			sinewave = SINE_S;
+			signal_q = SIG_S;
+			filtered= FIL_S;
+			lowpass = LOWPASS_S;
+			amplitude_temp = AMP_TEMP;
+			amplitude = AMP_S;
+			frequency = FREQ_S;
+			cutOffFreq = FGFREQ_S;
+			break;
+
+		case 'U':
+			if (strncmp(":0.1",strchr(response, ':'),4) == 0 ){
+				cutOffFreq = 0.1;
+			}
+			else if (strncmp(":0.2", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.2;
+			}
+			else if (strncmp(":0.3", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.3;
+			}
+			else if (strncmp(":0.4", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.4;
+			}
+			else if (strncmp(":0.5", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.5;
+			}
+			else if (strncmp(":0.6", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.6;
+			}
+			else if (strncmp(":0.7", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.7;
+			}
+			else if (strncmp(":0.8", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.8;
+			}
+			else if (strncmp(":0.9", strchr(response,':'),4) == 0){
+				cutOffFreq = 0.9;
+			}
+			break;
+
+		default:
+			break;
+	}
+}
